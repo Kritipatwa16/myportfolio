217c44fdf1ae809e77e07e848046389eb7ad182d
Kritipatwa16 <kriti.patwa15@gmail.com>
2025-11-02 02:22:53 +0530

    chamge in cv link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1WKDaarSfk0Zyu5ugMfyH61-9HtcbjC13/view?usp=sharing"
+            href="https://drive.google.com/file/d/187M6pMDtRQ9vl99--qJHiLEVHsFwRMod/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
