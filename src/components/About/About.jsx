9b761aca58331ebe2f71ac546aeda7c528156bd1
Kritipatwa16 <kriti.patwa15@gmail.com>
2026-05-08 21:51:08 +0530

    Changed resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1ilBj5TzbVo4cLueZKEycUtrWo3ceI3Wp/view?usp=sharing"
+            href="https://drive.google.com/file/d/1xBlZ7UjtWWZIqI87nNnk72aiDLnXPgsG/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
